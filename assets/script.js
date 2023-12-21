6328d12306ba9dea9be1fb746233a243d43011c0
unknown <whritenourc@outlook.com>
2023-12-20 18:46:41 -0600

    added all core functionality, I would have done more commits but a tutor helped me with this

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,74 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var timeTextEl = $('.time-text')
-
-var formatTimeBoxes = function () {
-timeTextEl.addClass('col-2 col-md-1 hour text-center py-3');
-}
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+var currentDayEl = $('#currentDay');
+var timeTextEl = $('.time-text');
+var textAreaEl = $('.text-area');
+var saveButtonEl = $('.saveBtn');
+var hour9El = $('#hour-9');
+var hour10El = $('#hour-10');
+var hour11El = $('#hour-11');
+var hour12El = $('#hour-12');
+var hour13El = $('#hour-13');
+var hour14El = $('#hour-14');
+var hour15El = $('#hour-15');
+var hour16El = $('#hour-16');
+var hour17El = $('#hour-17');
+var hoursEl = $('.hours');
+
+var hours = [hour9El, hour10El, hour11El, hour12El, hour13El, hour14El, hour15El, hour16El, hour17El]
+
+
+
+
+var curHour = dayjs().format('H');
+var curTime = dayjs().format('dddd MMMM D');
+
+var timeHeader = function() {
+currentDayEl.text(curTime)
+}
+timeHeader();
+
+
+var formatBoxes = function () {
+  timeTextEl.addClass('col-2 col-md-1 hour text-center py-3');
+  textAreaEl.addClass('col-8 col-md-10 description rows="3"');
+  saveButtonEl.addClass('col-2 col-md-1 aria-label="save"')
+  hoursEl.addClass('row time-block')
+};
+formatBoxes();
+
+var colorizeBoxes = function () {
+  for (let i = 0; i < hours.length; i++) {
+    if(curHour == hours[i].data('hour')) {
+      hours[i].addClass('present')
+    } else if (curHour > hours[i].data('hour')) {
+      hours[i].addClass('past')
+    } else  if (curHour < hours[i].data('hour')) {
+      hours[i].addClass('future')
+  }
+}};
+colorizeBoxes();
+
+var saveButton = function() {
+  saveButtonEl.on('click', function(event) {
+    var text = $(this).siblings('.text-area').val()
+    var hourID = $(this).parent().attr('id')
+    localStorage.setItem(hourID, text)
+  })
+}
+saveButton()
+
+var MemorySchedule = function() {
+  for (let i = 9; i < hours.length+9; i++) {
+    $('#hour-' + i).children('.text-area').val(localStorage.getItem('hour-'+i))
+  }
+}
+MemorySchedule()
 });
+
+
+
